c8ae08f31b5eca1759046e32d15b94cc69cb7b4f
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-04-29 19:49:15 +0530

    Create removeDuplicates.java

---
+++ removeDuplicates.java
@@ -0,0 +1,15 @@
+    public static void removeDuplicates(String str,int idx,StringBuilder newStr,boolean map[]){
+        //aapppna
+        if(idx==str.length()){
+            System.out.println(newStr);
+            return;
+        }
+        char currChar=str.charAt(idx);
+        if(map[currChar-'a']==true){
+            removeDuplicates(str,idx+1,newStr,map);
+        }else{
+            map[currChar-'a']=true;
+            removeDuplicates(str,idx+1,newStr.append(currChar),map);
+            
+        }
+    }
